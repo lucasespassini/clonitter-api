5bf19c4fc78e57c7e936fcf9d835820ac7d5ee44
Lucas Mendes <lucasespassini@gmail.com>
2022-08-15 16:57:08 -0300

    configurando upload de iamgem

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -6,10 +6,15 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
 
 @Controller('user')
 export class UsersController {
@@ -18,6 +23,24 @@ export class UsersController {
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
+  }
+
+  @Post('profile-image')
+  @UseInterceptors(
+    FileInterceptor('profile-image', {
+      storage: diskStorage({
+        destination: './uploads/profile-images',
+        filename: (req, file, cb) => {
+          const fileName = file.originalname + uuidv4();
+          const extension = '.jpg';
+
+          cb(null, `${fileName}${extension}`);
+        },
+      }),
+    }),
+  )
+  uploadAvatar(@UploadedFile() file: Express.Multer.File) {
+    return { imagePath: file };
   }
 
   @Get()
